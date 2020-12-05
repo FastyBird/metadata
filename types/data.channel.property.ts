ad989d1c8a635c014645b40f8f2b610a7773315d
Adam Kadlec <adam.kadlec@gmail.com>
2020-12-05 16:52:56 +0100

    Data exchange schemas

---
+++ types/data.channel.property.ts
@@ -0,0 +1,7 @@
+export interface Data {
+	device: string
+	channel: string
+	property: string
+	expected: string | number | boolean | null
+	[k: string]: string | number | boolean | null | undefined
+}
---
+++ types/data.device.property.ts
@@ -0,0 +1,6 @@
+export interface Data {
+	device: string
+	property: string
+	expected: string | number | boolean | null
+	[k: string]: string | number | boolean | null | undefined
+}
