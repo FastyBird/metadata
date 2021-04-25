f30ac207b12eeedbf9e39683014f6b52ed425cd7
Adam Kadlec <adam.kadlec@gmail.com>
2021-04-25 21:51:32 +0200

    Moving types from triggers module

---
+++ src/Types/TriggerConditionOperatorType.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+/**
+ * TriggerConditionOperatorType.php
+ *
+ * @license        More in license.md
+ * @copyright      https://www.fastybird.com
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ * @package        FastyBird:ModulesMetadata!
+ * @subpackage     Types
+ * @since          0.1.0
+ *
+ * @date           04.04.20
+ */
+
+namespace FastyBird\ModulesMetadata\Types;
+
+use Consistence;
+
+/**
+ * Trigger condition operator type
+ *
+ * @package        FastyBird:ModulesMetadata!
+ * @subpackage     Types
+ *
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ */
+class TriggerConditionOperatorType extends Consistence\Enum\Enum
+{
+
+	/**
+	 * Define states
+	 */
+	public const OPERATOR_VALUE_EQUAL = 'eq';
+	public const OPERATOR_VALUE_ABOVE = 'above';
+	public const OPERATOR_VALUE_BELOW = 'below';
+
+	/**
+	 * @return string
+	 */
+	public function __toString(): string
+	{
+		return (string) self::getValue();
+	}
+
+}
