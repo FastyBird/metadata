f0c9d3207ffd618d081464063129ba21217e4246
Adam Kadlec <adam.kadlec@gmail.com>
2021-04-30 00:44:53 +0200

    Reverting

--- modules-metadata.d.ts
+++ modules-metadata.d.ts
@@ -1,3 +1,6 @@
+export * from '@/lib/types/types'
+export * from '@/lib/types/routing'
+
 export * from '@/lib/types/accounts-module'
 export * from '@/lib/types/devices-module'
 export * from '@/lib/types/triggers-module'
