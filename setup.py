269cf945da763ff8befc516dcaebc136dd0a397e
Adam Kadlec <adam.kadlec@gmail.com>
2021-07-26 21:39:12 +0200

    Fixing path

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-VERSION: str = find_version("modules-metadata", "__init__.py")
+VERSION: str = find_version("modules_metadata", "__init__.py")
 
 setup(
     version=VERSION,
