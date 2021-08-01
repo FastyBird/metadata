b551a4b6a45301d127bf5d8b86c2d29e4fdffa08
Adam Kadlec <adam.kadlec@gmail.com>
2021-08-01 21:06:50 +0200

    Code cleaning

--- modules_metadata/version.py
+++ modules_metadata/version.py
@@ -23,10 +23,10 @@ from os.path import abspath, dirname, exists, join, normpath
 
 PACKAGE_PATH = abspath(dirname(__file__))
 
-DIST_SPEC = 'modules_metadata'
+DIST_SPEC = "modules_metadata"
 
 # Base version, which will be augmented with Git information
-BASE_VERSION = '0.3.0'
+BASE_VERSION = "0.3.2"
 
 # This string will be replaced by `git-archive`
 # with the abbreviated commit hash
@@ -37,13 +37,13 @@ def git_describe():
     from subprocess import check_call, check_output
 
     # Get the version from the local Git repository
-    check_call(['git', 'update-index', '-q', '--refresh'], cwd=PACKAGE_PATH)
+    check_call(["git", "update-index", "-q", "--refresh"], cwd=PACKAGE_PATH)
 
-    desc = check_output(['git', 'describe', '--long', '--dirty', '--tag'], cwd=PACKAGE_PATH)
-    desc = desc.decode('utf-8').strip()
+    desc = check_output(["git", "describe", "--long", "--dirty", "--tag"], cwd=PACKAGE_PATH)
+    desc = desc.decode("utf-8").strip()
 
-    tag, commits, rev = desc.split('-', 2)
-    tag = tag.lstrip('v')
+    tag, commits, rev = desc.split("-", 2)
+    tag = tag.lstrip("v")
     commits = int(commits)
 
     return tag, commits, rev
@@ -52,25 +52,25 @@ def git_describe():
 def get_version():
     # Git repo
     # If a local git repository is present, use `git describe` to provide a rich version
-    git_dir = normpath(join(PACKAGE_PATH, '.git'))
+    git_dir = normpath(join(PACKAGE_PATH, ".git"))
 
     if exists(git_dir):
         tag, commits, rev = git_describe()
 
         # Ensure the base version matches the Git tag
         if tag != BASE_VERSION:
-            raise Exception('Git revision different from base version')
+            raise Exception("Git revision different from base version")
 
         # No local version if we're on a tag
-        if commits == 0 and not rev.endswith('dirty'):
+        if commits == 0 and not rev.endswith("dirty"):
             return BASE_VERSION
 
-        return '{}+{}-{}'.format(BASE_VERSION, commits, rev)
+        return "{}+{}-{}".format(BASE_VERSION, commits, rev)
 
     # Git archive
     # If this was produced via `git archive`, we'll use the version it provides
-    if not git_archive_rev.startswith('$'):
-        return '{}+g{}'.format(BASE_VERSION, git_archive_rev)
+    if not git_archive_rev.startswith("$"):
+        return "{}+g{}".format(BASE_VERSION, git_archive_rev)
 
     # Package resource
     # Otherwise, we're either installed (e.g. via pip), or running from
@@ -84,12 +84,12 @@ def get_version():
         pass
 
     # This shouldn't be able to happen
-    sys.stderr.write('WARNING: Failed to determine version!\n')
+    sys.stderr.write("WARNING: Failed to determine version!\n")
 
     return BASE_VERSION
 
 
 __version__ = get_version()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(__version__)
