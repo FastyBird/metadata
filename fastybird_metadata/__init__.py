82a128bc247a2da27a0ef2135dadea1d7e59ebd0
Adam Kadlec <adam.kadlec@gmail.com>
2022-04-05 19:02:17 +0200

    Removing module specific value normalizer

--- fastybird_metadata/__init__.py
+++ fastybird_metadata/__init__.py
@@ -18,4 +18,4 @@
 Metadata loader and validator and sets of useful enums
 """
 
-__version__ = "0.51.0"
+__version__ = "0.52.0"
--- public/entry.ts
+++ public/entry.ts
@@ -1,5 +1,2 @@
-// Re-export plugin helpers
-export * from '@/lib/helpers'
-
 // Re-export plugin typing
 export * from '@/types/metadata'
